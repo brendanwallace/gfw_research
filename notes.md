33eaafbf3c29d3e865afdaa478234e76c9a71727
Brendan Wallace <brendan@Brendans-MacBook-Pro-2021.local>
2022-03-31 12:44:52 -0700

    Back up work

---
+++ notes.md
@@ -0,0 +1,38 @@
+## Findings
+
+Of 1055 "no take" zones, >1000 never saw any fishing effort between 2012 and 2020.
+
+27 of the \~50 with effort were implemented since 2012; about 10 of those showed
+a significant decrease in effort, 15 or so only had pretty marginal amounts of
+fishing effort, and I'd say 2 had high amounts of effort inside the areas even
+after being implemented.
+
+
+### Criteria for inclusion:
+ - total amount of fishing effort *and* percentage of 
+ fishing effort among affected ships decreased
+- one or both of these decreased by at least 20%
+Ross Sea
+Palau
+Moana Mahu (barely)
+Phoenix Island
+Terres Australes Françaises
+Coral Sea - National Park Zone
+Central Eastern - National Park Zone
+Revillagigedo - Zona Núcleo/Uso
+
+
+Of the other 25 MPAs that saw fishing effort but were implemented before 2012,
+most again only had pretty marginal effort inside them, with a handful of
+exceptions (about 5).
+
+1. Revillagigedo
+2. Norfolk - National Park Zone
+3. Natural Park of the Coral Sea
+4. Moana Mahu
+5. Phoenix Island Protected Area
+6. Perth Canyon - National Park Zone
+7. South-west Corner - National Park Zone
+8. Ross Sea
+9. Palau
+10. Kimberley - National Park Zone
--- util/analyze.py
+++ util/analyze.py
@@ -11,12 +11,21 @@ from shapely import ops
 
 """
 NOTE: just changed 'op=...' to 'predicate=...' because of this:
-FutureWarning: The `op` parameter is deprecated and will be removed in a future release. Please use the `predicate` parameter instead.
+FutureWarning: The `op` parameter is deprecated and will be removed in a future release.
+Please use the `predicate` parameter instead.
   pipa_res = util.analyze_mpa(geopoints_sampled, points_by_year, pipa, '2013-01-01')
 """
 
+class MpaResults():
+    """
+    Container class for all the intermediate results of different analysis
+    """
+    def __init__(self, mpa_frame, date, name, ):
+        pass
+
 def join_on_lat_lon(points):
-    return points.groupby(['cell_ll_lat', 'cell_ll_lon'], as_index=False).aggregate({'cell_ll_lat': 'first', 'cell_ll_lon': 'first', 'fishing_hours': 'sum'})
+    return points.groupby(['cell_ll_lat', 'cell_ll_lon'], as_index=False).aggregate(
+        {'cell_ll_lat': 'first', 'cell_ll_lon': 'first', 'fishing_hours': 'sum'})
 
 
 ## Compute geometries for the points
@@ -34,7 +43,8 @@ def convert_to_geo(points, box=True):
     else:
         return geopandas.GeoDataFrame(
             points,
-            geometry=geopandas.points_from_xy(points['cell_ll_lon']*0.1 + 0.05, points['cell_ll_lat']*0.1 + 0.05)
+            geometry=geopandas.points_from_xy(
+                points['cell_ll_lon']*0.1 + 0.05, points['cell_ll_lat']*0.1 + 0.05)
         ).set_crs(epsg=4326)
 
 
@@ -50,30 +60,53 @@ def table_of_in_out_pre_post(points_of_mpa_ships, mpa, date):
     geopoints_ = convert_to_geo(points_of_mpa_ships, box=True)
     geopoints_['pre'] = geopoints_['date'] < date
     # fastest way to do this is just this whole join:
-    geopoints_['in_mpa'] = ~geopandas.sjoin(geopoints_, mpa[['geometry']], how='left', predicate='within')['index_right'].isnull()
+    geopoints_['in_mpa'] = ~geopandas.sjoin(
+        geopoints_, mpa[['geometry']], how='left', predicate='within')['index_right'].isnull()
     
 
     # TODO - what about on the border/indeterminate?
-    # geopoints_['intersects_mpa'] = ~geopandas.sjoin(geopoints_, mpa[['geometry']], how='left', predicate='intersects')['index_right'].isnull()
-    aggregated_ = geopoints_.groupby(['mmsi', 'in_mpa', 'pre'], as_index=False).aggregate({'fishing_hours': 'sum', 'mmsi': 'first'})
-    aggregated_['in_pre'] = aggregated_.apply(lambda row: row['fishing_hours'] if (row['in_mpa'] and row['pre']) else 0.0, axis=1)
-    aggregated_['out_pre'] = aggregated_.apply(lambda row: row['fishing_hours'] if (not row['in_mpa'] and row['pre']) else 0.0, axis=1)
-    aggregated_['in_post'] = aggregated_.apply(lambda row: row['fishing_hours'] if (row['in_mpa'] and not row['pre']) else 0.0, axis=1)
-    aggregated_['out_post'] = aggregated_.apply(lambda row: row['fishing_hours'] if (not row['in_mpa'] and not row['pre']) else 0.0, axis=1)
-    table = aggregated_.groupby(['mmsi'], as_index=False).aggregate({'in_pre': 'sum', 'out_pre': 'sum', 'in_post': 'sum', 'out_post': 'sum'})
+    # geopoints_['intersects_mpa'] = ~geopandas.sjoin(
+    #   geopoints_, mpa[['geometry']], how='left', predicate='intersects')['index_right'].isnull()
+    aggregated_ = geopoints_.groupby(['mmsi', 'in_mpa', 'pre'], as_index=False).aggregate(
+        {'fishing_hours': 'sum', 'mmsi': 'first'})
+    aggregated_['in_pre'] = aggregated_.apply(
+        lambda row: row['fishing_hours'] if (row['in_mpa'] and row['pre']) else 0.0, axis=1)
+    aggregated_['out_pre'] = aggregated_.apply(
+        lambda row: row['fishing_hours'] if (not row['in_mpa'] and row['pre']) else 0.0, axis=1)
+    aggregated_['in_post'] = aggregated_.apply(
+        lambda row: row['fishing_hours'] if (row['in_mpa'] and not row['pre']) else 0.0, axis=1)
+    aggregated_['out_post'] = aggregated_.apply(
+        lambda row: row['fishing_hours'] if (not row['in_mpa'] and not row['pre']) else 0.0, axis=1)
+    table = aggregated_.groupby(['mmsi'], as_index=False).aggregate(
+        {'in_pre': 'sum', 'out_pre': 'sum', 'in_post': 'sum', 'out_post': 'sum'})
     table['pre_percent_in'] = table['in_pre'] / (table['in_pre'] + table['out_pre'])
     table['post_percent_in'] = table['in_post'] / (table['in_post'] + table['out_post'])
-    return table
+    return table, geopoints_
 
    
 
-def plot_effort_with_world(effort, mpa, linewidth=0.5, title=''):
+def plot_effort_with_world(effort, mpa, linewidth=0.5, title='', filename=None):
+    fig = plt.figure()
     world = geopandas.read_file(geopandas.datasets.get_path('naturalearth_lowres'))
     ax = world.plot(color='white', edgecolor='black', figsize=(20, 30))
     effort.plot(column='fishing_hours', cmap='Blues', scheme='quantiles', ax=ax, legend=True)
     mpa.plot(ax=ax, color='None', edgecolor='red', linewidth=linewidth, alpha=0.5)
     plt.title(title)
+    if filename:
+        plt.savefig(filename)
+        #pl.clf()
+    plt.close(fig)
 
+def plot_effort_without_world(effort, mpa, linewidth=0.5, title='', filename=None):
+    fig = plt.figure()
+    world = geopandas.read_file(geopandas.datasets.get_path('naturalearth_lowres'))
+    ax = effort.plot(column='fishing_hours', cmap='Blues', scheme='quantiles', legend=True)
+    mpa.plot(ax=ax, color='None', edgecolor='red', linewidth=linewidth, alpha=0.5)
+    plt.title(title)
+    if filename:
+        plt.savefig(filename)
+        #pl.clf()
+    plt.close(fig)
 
 def points_of_interest(geopoints_sampled, points_by_year, mpa, date, verbose=False):
     if verbose:
@@ -101,23 +134,130 @@ def points_of_interest(geopoints_sampled, points_by_year, mpa, date, verbose=Fal
     return points_of_mpa_ships
 
 
-def analyze_mpa(geopoints_sampled, points_by_year, mpa, date, plot_pre_post=True, verbose=False):
+def plot_in_out_lines(points_of_mpa_ships, mpa_name, date, window=30, folder=None, individuals=False):
+    start = np.datetime64('2012-01-01')
+    end = np.datetime64('2021-01-01')
+    days_in_interval = (end - start).item().days
+    X = np.arange(start + np.timedelta64(window-1, 'D'), end, np.timedelta64(1, 'D')).astype(datetime)
+
+
+    def _avg(x, w):
+        return np.convolve(x, np.ones(w), 'valid') / w
+
+    def _plot(points, filename, title):
+        effort_in = np.zeros(days_in_interval)
+        effort_out = np.zeros(days_in_interval)
+        for i, row in points.iterrows():
+            d = (row['date'] - start).days
+            if row['in_mpa']:
+                effort_in[d] += row['fishing_hours']
+            else:
+                effort_out[d] += row['fishing_hours']
+        fig = plt.figure()
+        plt.plot(X, _avg(effort_in, window), label='in')
+        plt.plot(X, _avg(effort_out, window), label='out')
+        plt.axvline(np.datetime64(date), c='black', ls=':')
+        plt.ylabel(f'fishing hours – {window}-day average')
+        plt.legend(loc='upper right')
+        plt.title(title)
+        plt.savefig(filename)
+        plt.close(fig)
+
+    if individuals:
+        mmsis = points_of_mpa_ships['mmsi'].unique()
+        for mmsi in mmsis:
+            points = points_of_mpa_ships[points_of_mpa_ships['mmsi'] == mmsi]
+            filename = folder+'/by_mmsi/'+str(mmsi)
+            _plot(points, filename, f'{mmsi} – {mpa_name}')
+
+    _plot(points_of_mpa_ships, folder+f'/{mpa_name}', f'{mpa_name}')
+    
+
+def draw_maps(points_of_mpa_ships, mpa_name, date, mpa, folder=None, individuals=False):
+
+    def _draw(points, filename, title):
+        pre, post = points[points['date'] < date], points[points['date'] >= date]
+        if pre.shape[0] > 0:
+            pre = convert_to_geo(join_on_lat_lon(pre), box=True)
+            plot_effort_with_world(pre, mpa, title=title+' pre',
+                    filename=filename+'_pre')
+        if post.shape[0] > 0:
+            pre = convert_to_geo(join_on_lat_lon(post), box=True)
+            plot_effort_with_world(post, mpa, title=title+' post',
+                    filename=filename+'_post')
+
+
+    if individuals:
+        mmsis = points_of_mpa_ships['mmsi'].unique()
+        for mmsi in mmsis:
+            points = points_of_mpa_ships[points_of_mpa_ships['mmsi'] == mmsi]
+            filename = folder+'/by_mmsi/'+str(mmsi)+'_map'
+            _draw(points, filename, f'{mmsi} – {mpa_name}')
+
+
+
+def analyze_mpa(geopoints_sampled, points_by_year, mpa, date, name,
+    plot=False, verbose=False, folder=None, total_effort_plots=False):
     #mpa = mpa.dissolve(by='WDPAID')
     #mpa = mpa.dissolve(by='wdpa_id')
 
-    points_of_mpa_ships = points_of_interest(geopoints_sampled, points_by_year, mpa, date, verbose=verbose)
+    points_of_mpa_ships = points_of_interest(
+        geopoints_sampled, points_by_year, mpa, date, verbose=verbose)
     if points_of_mpa_ships is None:
         return
 
-    table = table_of_in_out_pre_post(points_of_mpa_ships, mpa, date,).sort_values('in_pre', ascending=False)
+    table, points_of_mpa_ships = table_of_in_out_pre_post(
+        points_of_mpa_ships, mpa, date,)
+    table = table.sort_values('in_pre', ascending=False)
+
+
     
-    pre = convert_to_geo(
-        join_on_lat_lon(points_of_mpa_ships[points_of_mpa_ships['date'] < date]), box=True)
-    post = convert_to_geo(
-        join_on_lat_lon(points_of_mpa_ships[points_of_mpa_ships['date'] >= date]), box=True)
-    
-    if plot_pre_post:
-        plot_effort_with_world(pre, mpa, title='effort pre-closure')
-        plot_effort_with_world(post, mpa, title='effort post-closure')
- 
-    return table, pre, post, points_of_mpa_ships
+    if plot:
+        try:
+            os.makedirs(folder)
+            #os.makedirs(folder+'/by_mmsi')
+        except:
+            pass
+
+        plot_in_out_lines(points_of_mpa_ships, name, date, folder=folder)
+        draw_maps(points_of_mpa_ships, name, date, mpa, folder=folder)
+
+
+        pre = points_of_mpa_ships[points_of_mpa_ships['date'] < date]
+        post = points_of_mpa_ships[points_of_mpa_ships['date'] >= date]
+        if total_effort_plots:
+            if (pre.shape[0] > 0):
+                pre = convert_to_geo(
+                    join_on_lat_lon(points_of_mpa_ships[points_of_mpa_ships['date'] < date]), box=True)
+                filename=None
+                if folder:
+                    plot_effort_with_world(pre, mpa, title=f'{name} effort pre-closure ({date})',
+                        filename=folder+'pre_closure_effort_with_world')
+                    plot_effort_without_world(pre, mpa, title=f'{name} effort pre-closure ({date})',
+                        filename=folder+'pre_closure_effort')
+
+            if (post.shape[0] > 0):
+                post = convert_to_geo(
+                    join_on_lat_lon(points_of_mpa_ships[points_of_mpa_ships['date'] >= date]), box=True)
+                filename=None
+                if folder:
+                    plot_effort_with_world(post, mpa, title=f'{name} effort post-closure ({date})',
+                        filename=folder+'post_closure_effort_with_world')
+                    plot_effort_without_world(post, mpa, title=f'{name} effort post-closure ({date})',
+                        filename=folder+'post_closure_effort')
+            
+
+
+
+    return table, points_of_mpa_ships
+
+
+
+
+
+
+
+
+
+
+
